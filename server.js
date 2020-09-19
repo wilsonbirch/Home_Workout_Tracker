30d25b3bdc45178e7789a470bb27167d92d5b84e
Wilson Birch <wilsonbirch@gmail.com>
2020-09-19 14:42:04 -0400

    added routes for the server.js file to navigate to each html page

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
+const db = require("./models");
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -10,6 +12,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+app.get('/stats', (req,res) => {
+  res.redirect('stats.html');
+})
+
+app.get('/exercise', (req,res) => {
+  res.redirect('exercise.html');
+})
+
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
+    console.log(`App running on port localhost:${PORT}!`);
   });
